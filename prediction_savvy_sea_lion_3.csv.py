496138bfbb39e45b8b43d97facf923a0bacf94db
DESKTOP-J8M4ELN\22825 <JoySun678@gmail.com>
2022-01-14 08:49:32 +0100

    code clean

--- prediction_savvy_sea_lion_3.csv.py
+++ prediction_savvy_sea_lion_3.csv.py
@@ -3,38 +3,31 @@ the classification/regression part is partially quoted from the source
 https://stackabuse.com/random-forest-algorithm-with-python-and-scikit-learn/
 with regards and thanks to the tutorial of usage of sklearning package posts
 """
+# Loading packages and data
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-# random seed format
+# Set a fixed random seed to make the work reproducible
 np.random.seed(2022)
 
 transactions = pd.read_csv("transactions.csv")
 customers = pd.read_csv("customers.csv")
 geo = pd.read_csv("geo.csv")
 
+# Join transactions with geo, customers
+# We use the left outer join to ensure there is no data loss in transactions
 df = transactions.merge(geo, how="left")
-df["COUNTRY"] = df["COUNTRY"].map({"CH": "Switzerland", "FR": "France"})
-
-# data preparation for OFFER_STATUS
-df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["LOST", "Lost", "LOsT", "Lose"], 0.)
-df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["WIN", "Win", "Won", "WON"], 1.)
-
-# converting the date
-df["MO_CREATED_DATE"] = pd.to_datetime(df["MO_CREATED_DATE"], errors='ignore')
-df["SO_CREATED_DATE"] = pd.to_datetime(df["SO_CREATED_DATE"], errors='ignore')
 
 
-# converting customer ids to nums
+# The data type of attribute "CUSTOMER" in data transactions is String
+# We need to firstly convert the data type into int
+
+# Converting string to nums
 def remove_quote(entry):
     return entry[1:-1]
-
-
-# merging 3 lists
-df["CUSTOMER"] = df["CUSTOMER"].map(remove_quote, 'ignore')
 
 
 def remove_NV(entry):
@@ -44,34 +37,49 @@ def remove_NV(entry):
         return float(entry)
 
 
+df["CUSTOMER"] = df["CUSTOMER"].map(remove_quote, 'ignore')
 df["CUSTOMER"] = df["CUSTOMER"].map(remove_NV).dropna().astype(int)
 customers = customers.dropna()
+
+# The data type of "REV_CURRENT_YEAR" is string
+# We need to convert the data type into float
 customers["REV_CURRENT_YEAR"] = customers["REV_CURRENT_YEAR"].map(remove_quote).astype(float)
 
-# left join a second list
+# The format of attribute "COUNTRY" is different in data customers and transactions
+# We need to firstly convert them to the same format
+df["COUNTRY"] = df["COUNTRY"].map({"CH": "Switzerland", "FR": "France"})
 df = df.merge(customers, how="left", on=["CUSTOMER", "COUNTRY"])
+
+# Exploratory Analysis and Preprocessing
+
+# data preparation for OFFER_STATUS
+df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["LOST", "Lost", "LOsT", "Lose"], 0.)
+df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["WIN", "Win", "Won", "WON"], 1.)
+
 df = df[df["CUSTOMER"].notna()]
 df["ISIC"] = df["ISIC"].fillna(df["ISIC"].mean())
 
-# dealing with the dates
+# Dealing with the dates
 df["CREATION_YEAR"] = pd.to_datetime(df["CREATION_YEAR"]).dt.year
 df["MO_CREATED_DATE"] = pd.to_datetime(df["MO_CREATED_DATE"]).dt.month
 df["SO_CREATED_DATE"] = pd.to_datetime(df["SO_CREATED_DATE"]).dt.month
 
 df["CREATION_YEAR"] = df["CREATION_YEAR"].fillna(df["CREATION_YEAR"].mean())
 
+# Uniting currency with cny
 df["CURRENCY"] = df["CURRENCY"].map({"Chinese Yuan": 1, "Euro": 7.25, "US Dollar": 6.36, "Pound Sterling": 8.72, 0: 0})
 for col in ["CURRENCY", "REV_CURRENT_YEAR", "REV_CURRENT_YEAR.1", "REV_CURRENT_YEAR.2"]:
     df[col] = df[col].fillna(df[col].mean())
 
+df["REV_CURRENT_YEAR"] = df["REV_CURRENT_YEAR"] * df["CURRENCY"]
+df["REV_CURRENT_YEAR.1"] = df["REV_CURRENT_YEAR.1"] * df["CURRENCY"]
+df["REV_CURRENT_YEAR.2"] = df["REV_CURRENT_YEAR.2"] * df["CURRENCY"]
+
 df["COUNTRY"] = df["COUNTRY"].fillna("UNKNOWN")
 df["OWNERSHIP"] = df["OWNERSHIP"].fillna("UNKNOWN")
 
-# useless variables
-df = df.drop(columns=["MO_ID", "SO_ID", "SALES_LOCATION",
-             "SALES_OFFICE"])
 
-# end customers
+# Process the end_customers
 def map_end_customer(entry):
     if entry in ["No", np.nan]:
         return 0
@@ -80,50 +88,49 @@ def map_end_customer(entry):
     else:
         return 1
 
+
 df["END_CUSTOMER"] = df["END_CUSTOMER"].map(map_end_customer)
 
-# uniting currency with cny
-
-df["REV_CURRENT_YEAR"] = df["REV_CURRENT_YEAR"] * df["CURRENCY"]
-df["REV_CURRENT_YEAR.1"] = df["REV_CURRENT_YEAR.1"] * df["CURRENCY"]
-df["REV_CURRENT_YEAR.2"] = df["REV_CURRENT_YEAR.2"] * df["CURRENCY"]
-df = df.drop("CURRENCY", 1)
-# turning high/low margin products to pertage and comparable(effective)
+# Turning high/low margin products to percentage and make them comparable(effective)
 for letter in ["A", "B", "C", "D", "E"]:
     df["Percentage_of_Product_" + letter] = df["COSTS_PRODUCT_" + letter] / (df["OFFER_PRICE"])
     df = df.drop(columns=["COSTS_PRODUCT_" + letter])
 
+# One-hot encoding for nominal
+df = pd.get_dummies(df, columns=["TECH", "BUSINESS_TYPE", "PRICE_LIST", "OWNERSHIP", "OFFER_TYPE", "COUNTRY",
+                                 "SALES_BRANCH"])
 
-# one-hot encoding for nominal
-df = pd.get_dummies(df, columns=["TECH", "BUSINESS_TYPE", "PRICE_LIST", "OWNERSHIP", "OFFER_TYPE", "COUNTRY","SALES_BRANCH"])
-# TODO: a couple of variables deleted in the current model, to be handled or deleted
-df = df.drop(columns=["CUSTOMER"])
+# Drop useless variables
+df = df.drop(columns=["MO_ID", "SO_ID", "SALES_LOCATION",
+                      "SALES_OFFICE", "CURRENCY", "CUSTOMER"])
 
-train_set = df[df["OFFER_STATUS"].notna()]
-test_set_init = df[df["OFFER_STATUS"].isna()]
+# Modeling the data
 
-train_set = train_set.drop("TEST_SET_ID", 1)
-test_set = test_set_init.drop("TEST_SET_ID", 1)
+# separate the training set and the test set
+train_set = df[df["OFFER_STATUS"].notna()].drop(columns="TEST_SET_ID")
+test_set_init = df[df["OFFER_STATUS"].isna()]  # used to generate the outcome
 
-# dividing the outcomes and variables
+test_set = test_set_init.drop(columns="TEST_SET_ID")
+
+# Dividing the variables and outcomes
 Y_train = train_set["OFFER_STATUS"].values
-X_train = train_set.drop("OFFER_STATUS", 1).values
+X_train = train_set.drop(columns="OFFER_STATUS").values
 
-Y_test = test_set["OFFER_STATUS"].values
-X_test = test_set.drop("OFFER_STATUS", 1).values
+X_test = test_set.drop(columns="OFFER_STATUS").values
 
-# the scaling matter, for good habits
+# The scaling matter, for good habits
 sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
-# training and fit the best tree in the model
+# Training and fit the best tree in the model
 classifier = RandomForestClassifier(n_estimators=100, class_weight="balanced_subsample")
 classifier.fit(X_train, Y_train)
 Y_pred = classifier.predict(X_test)
 
-# output the results
-result = pd.DataFrame([test_set_init["TEST_SET_ID"].to_numpy(), np.split(Y_pred, len(Y_pred))], index=["id", "prediction"]).T
+# Output the results
+result = pd.DataFrame([test_set_init["TEST_SET_ID"].to_numpy(), np.split(Y_pred, len(Y_pred))],
+                      index=["id", "prediction"]).T
 result["prediction"] = result["prediction"].map(np.sum)
 result = result.astype(int)
 result.to_csv("prediction_savvy_sea_lion_3.csv", index=False)
--- tester.py
+++ tester.py
@@ -3,38 +3,31 @@ the classification/regression part is partially quoted from the source
 https://stackabuse.com/random-forest-algorithm-with-python-and-scikit-learn/
 with regards and thanks to the tutorial of usage of sklearning package posts
 """
+# Loading packages and data
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-# random seed format
+# Set a fixed random seed to make the work reproducible
 np.random.seed(2022)
 
 transactions = pd.read_csv("transactions.csv")
 customers = pd.read_csv("customers.csv")
 geo = pd.read_csv("geo.csv")
 
+# Join transactions with geo, customers
+# We use the left outer join to ensure there is no data loss in transactions
 df = transactions.merge(geo, how="left")
-df["COUNTRY"] = df["COUNTRY"].map({"CH": "Switzerland", "FR": "France"})
-
-# data preparation for OFFER_STATUS
-df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["LOST", "Lost", "LOsT", "Lose"], 0.)
-df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["WIN", "Win", "Won", "WON"], 1.)
-
-# converting the date
-df["MO_CREATED_DATE"] = pd.to_datetime(df["MO_CREATED_DATE"], errors='ignore')
-df["SO_CREATED_DATE"] = pd.to_datetime(df["SO_CREATED_DATE"], errors='ignore')
 
 
-# converting customer ids to nums
+# The data type of attribute "CUSTOMER" in data transactions is String
+# We need to firstly convert the data type into int
+
+# Converting string to nums
 def remove_quote(entry):
     return entry[1:-1]
-
-
-# merging 3 lists
-df["CUSTOMER"] = df["CUSTOMER"].map(remove_quote, 'ignore')
 
 
 def remove_NV(entry):
@@ -44,34 +37,49 @@ def remove_NV(entry):
         return float(entry)
 
 
+df["CUSTOMER"] = df["CUSTOMER"].map(remove_quote, 'ignore')
 df["CUSTOMER"] = df["CUSTOMER"].map(remove_NV).dropna().astype(int)
 customers = customers.dropna()
+
+# The data type of "REV_CURRENT_YEAR" is string
+# We need to convert the data type into float
 customers["REV_CURRENT_YEAR"] = customers["REV_CURRENT_YEAR"].map(remove_quote).astype(float)
 
-# left join a second list
+# The format of attribute "COUNTRY" is different in data customers and transactions
+# We need to firstly convert them to the same format
+df["COUNTRY"] = df["COUNTRY"].map({"CH": "Switzerland", "FR": "France"})
 df = df.merge(customers, how="left", on=["CUSTOMER", "COUNTRY"])
+
+# Exploratory Analysis and Preprocessing
+
+# data preparation for OFFER_STATUS
+df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["LOST", "Lost", "LOsT", "Lose"], 0.)
+df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["WIN", "Win", "Won", "WON"], 1.)
+
 df = df[df["CUSTOMER"].notna()]
 df["ISIC"] = df["ISIC"].fillna(df["ISIC"].mean())
 
-# dealing with the dates
+# Dealing with the dates
 df["CREATION_YEAR"] = pd.to_datetime(df["CREATION_YEAR"]).dt.year
 df["MO_CREATED_DATE"] = pd.to_datetime(df["MO_CREATED_DATE"]).dt.month
 df["SO_CREATED_DATE"] = pd.to_datetime(df["SO_CREATED_DATE"]).dt.month
 
 df["CREATION_YEAR"] = df["CREATION_YEAR"].fillna(df["CREATION_YEAR"].mean())
 
+# Uniting currency with cny
 df["CURRENCY"] = df["CURRENCY"].map({"Chinese Yuan": 1, "Euro": 7.25, "US Dollar": 6.36, "Pound Sterling": 8.72, 0: 0})
 for col in ["CURRENCY", "REV_CURRENT_YEAR", "REV_CURRENT_YEAR.1", "REV_CURRENT_YEAR.2"]:
     df[col] = df[col].fillna(df[col].mean())
 
+df["REV_CURRENT_YEAR"] = df["REV_CURRENT_YEAR"] * df["CURRENCY"]
+df["REV_CURRENT_YEAR.1"] = df["REV_CURRENT_YEAR.1"] * df["CURRENCY"]
+df["REV_CURRENT_YEAR.2"] = df["REV_CURRENT_YEAR.2"] * df["CURRENCY"]
+
 df["COUNTRY"] = df["COUNTRY"].fillna("UNKNOWN")
 df["OWNERSHIP"] = df["OWNERSHIP"].fillna("UNKNOWN")
 
-# useless variables
-df = df.drop(columns=["MO_ID", "SO_ID", "SALES_LOCATION",
-             "SALES_OFFICE"])
 
-# end customers
+# Process the end_customers
 def map_end_customer(entry):
     if entry in ["No", np.nan]:
         return 0
@@ -80,32 +88,28 @@ def map_end_customer(entry):
     else:
         return 1
 
+
 df["END_CUSTOMER"] = df["END_CUSTOMER"].map(map_end_customer)
 
-# uniting currency with cny
-
-df["REV_CURRENT_YEAR"] = df["REV_CURRENT_YEAR"] * df["CURRENCY"]
-df["REV_CURRENT_YEAR.1"] = df["REV_CURRENT_YEAR.1"] * df["CURRENCY"]
-df["REV_CURRENT_YEAR.2"] = df["REV_CURRENT_YEAR.2"] * df["CURRENCY"]
-df = df.drop("CURRENCY", 1)
-# turning high/low margin products to pertage and comparable(effective)
+# Turning high/low margin products to percentage and make them comparable(effective)
 for letter in ["A", "B", "C", "D", "E"]:
     df["Percentage_of_Product_" + letter] = df["COSTS_PRODUCT_" + letter] / (df["OFFER_PRICE"])
     df = df.drop(columns=["COSTS_PRODUCT_" + letter])
 
+# One-hot encoding for nominal
+df = pd.get_dummies(df, columns=["TECH", "BUSINESS_TYPE", "PRICE_LIST", "OWNERSHIP", "OFFER_TYPE", "COUNTRY",
+                                 "SALES_BRANCH"])
 
-# one-hot encoding for nominal
-df = pd.get_dummies(df, columns=["TECH", "BUSINESS_TYPE", "PRICE_LIST", "OWNERSHIP", "OFFER_TYPE", "COUNTRY","SALES_BRANCH"])
+# Drop useless variables
+df = df.drop(columns=["MO_ID", "SO_ID", "SALES_LOCATION",
+                      "SALES_OFFICE", "CURRENCY", "CUSTOMER", "TEST_SET_ID"])
 
-# TODO: a couple of variables deleted in the current model, to be handled or deleted
-df = df.drop(columns=["CUSTOMER", "TEST_SET_ID"])
-
-df = df[df["OFFER_STATUS"].notna()]
-
-Y = df["OFFER_STATUS"]
-X = df.drop("OFFER_STATUS", 1)
+# Modeling the data
 
 # dividing the outcomes and variables
+df = df[df["OFFER_STATUS"].notna()]
+Y = df["OFFER_STATUS"]
+X = df.drop(columns="OFFER_STATUS")
 X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=0)
 
 # the scaling matter, for good habits
