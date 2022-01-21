078973f977b2a79089441a17f25382f5f9266dca
Zixuan Fan <ge43yeb@mytum.de>
2022-01-21 19:08:01 +0100

    improve with imbalance-learn

--- preidction_savvy_sea_lion_7.py
+++ preidction_savvy_sea_lion_7.py
@@ -78,7 +78,6 @@ df["OFFER_STATUS"] = df["OFFER_STATUS"].replace(["WIN", "Win", "Won", "WON"], 1.
 # Dealing with the dates
 df["CREATION_YEAR"] = pd.to_datetime(df["CREATION_YEAR"]).dt.year
 df["CREATION_YEAR"] = df["CREATION_YEAR"].fillna(df["CREATION_YEAR"].mean())
-# TODO: DATE MATTER
 
 df["MO_CREATED_YEAR"] = pd.to_datetime(df["MO_CREATED_DATE"]).dt.year
 df["MO_CREATED_MONTH"] = pd.to_datetime(df["MO_CREATED_DATE"]).dt.month
@@ -97,7 +96,7 @@ df["REV_CURRENT_YEAR.1"] = df["REV_CURRENT_YEAR.1"] * df["CURRENCY"]
 df["REV_CURRENT_YEAR.2"] = df["REV_CURRENT_YEAR.2"] * df["CURRENCY"]
 
 df["REVENUE"] = (df["OFFER_PRICE"]-df["SERVICE_LIST_PRICE"]-df["MATERIAL_COST"]-df["SERVICE_COST"])/df["OFFER_PRICE"]
-#df["BARGAIN"] = (df["OFFER_PRICE"]-df["SERVICE_LIST_PRICE"])/df["OFFER_PRICE"]
+
 
 # Better to use unknown
 df["COUNTRY"] = df["COUNTRY"].fillna("UNKNOWN")
@@ -106,137 +105,51 @@ df["OWNERSHIP"] = df["OWNERSHIP"].fillna("UNKNOWN")
 df["END_CUSTOMER"] = df["END_CUSTOMER"].map(map_end_customer)
 df["ISIC"] = (df["ISIC"].fillna(0)/100)
 
-"""
-def map_isic(entry):
-    if entry in range(1, 4):
-        return 'A'
-    elif entry in range(5, 10):
-        return 'B'
-    elif entry in range(10, 34):
-        return 'C'
-    elif entry == 35:
-        return 'D'
-    elif entry in range(36, 40):
-        return 'E'
-    elif entry in range(41, 44):
-        return 'F'
-    elif entry in range(45, 48):
-        return 'G'
-    elif entry in range(49, 54):
-        return 'H'
-    elif entry in range(55, 57):
-        return 'I'
-    elif entry in range(58, 64):
-        return 'J'
-    elif entry in range(64, 67):
-        return 'K'
-    elif entry == 68:
-        return 'L'
-    elif entry in range(69, 76):
-        return 'M'
-    elif entry in range(77, 83):
-        return 'N'
-    elif entry == 84:
-        return 'O'
-    elif entry == 85:
-        return 'P'
-    elif entry in range(85, 89):
-        return 'Q'
-    elif entry in range(90, 94):
-        return 'R'
-    elif entry in range(94, 97):
-        return 'S'
-    elif entry in range(97, 99):
-        return 'T'
-    elif entry == 99:
-        return 'U'
-    else:
-        return "OMEGA"
-"""
-
 # One-hot encoding is bad in random forest, alternatively label encoding is better
 
 label_encoder = LabelEncoder()
 for feature in ["TECH", "BUSINESS_TYPE", "PRICE_LIST", "OWNERSHIP", "OFFER_TYPE", "SALES_BRANCH",
                 "SALES_LOCATION", "CURRENCY"]:
     df[feature] = label_encoder.fit_transform(df[feature])
-"""
-
-df = pd.get_dummies(df, columns=["TECH", "BUSINESS_TYPE",
-                                 "PRICE_LIST", "OWNERSHIP", "OFFER_TYPE",
-                                 "SALES_BRANCH", "SALES_LOCATION"])
-"""
-
 
 # Drop useless variables
-df = df.drop(columns=["MO_ID", "SO_ID", "CUSTOMER", "TEST_SET_ID", "MO_CREATED_DATE", "SO_CREATED_DATE", "END_CUSTOMER",
+df = df.drop(columns=["MO_ID", "SO_ID", "CUSTOMER", "MO_CREATED_DATE", "SO_CREATED_DATE", "END_CUSTOMER",
                       "COUNTRY", "SALES_OFFICE"])
 
 # Modeling the data
 
-# dividing the outcomes and variables
-df = df[df["OFFER_STATUS"].notna()]
-Y = df["OFFER_STATUS"]
-X = df.drop(columns="OFFER_STATUS")
-X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.1, random_state=random_state)
+# separate the training set and the test set
+train_set = df[df["OFFER_STATUS"].notna()].drop(columns="TEST_SET_ID")
+test_set_init = df[df["OFFER_STATUS"].isna()]  # used to generate the outcome
+
+test_set = test_set_init.drop(columns="TEST_SET_ID")
+
+# Dividing the variables and outcomes
+Y_train = train_set["OFFER_STATUS"].values
+X_train = train_set.drop(columns="OFFER_STATUS").values
+
+X_test = test_set.drop(columns="OFFER_STATUS").values
 
 # the scaling matter, for good habits
-sc = MinMaxScaler()
+sc = StandardScaler()
 X_train = sc.fit_transform(X_train)
 X_test = sc.transform(X_test)
 
 # training and fit the best tree in the model
 rf = BalancedRandomForestClassifier(n_estimators=128, class_weight="balanced_subsample", sampling_strategy="all",
-                                    criterion='entropy', random_state=random_state)
-logit = LogisticRegression(max_iter=2000, class_weight={1: 1, 0: 4.5}, solver="saga", C=0.01)
-et = ExtraTreesClassifier(n_estimators=200, class_weight="balanced_subsample", random_state=random_state)
+                                    criterion='entropy')
 
-voting = VotingClassifier(estimators=[('rf', rf), ('et', et)], voting="hard")
+rf.fit(X_train, Y_train)
 
-classifier = rf
-classifier.fit(X_train, Y_train)
-# cross validation
-# cv_results = cross_validate(classifier, X, Y, cv=10, return_train_score=True)
-# print(cv_results['test_score'])
+Y_pred = rf.predict(X_test)
 
-Y_pred = classifier.predict(X_test)
-
-from sklearn.metrics import classification_report, confusion_matrix, precision_score, recall_score, \
-    balanced_accuracy_score, accuracy_score
-
-print(confusion_matrix(Y_test, Y_pred))
-print(classification_report(Y_test, Y_pred))
-
-# GridSearch for paras
+# Output the results
+result = pd.DataFrame([test_set_init["TEST_SET_ID"].to_numpy(), np.split(Y_pred, len(Y_pred))],
+                      index=["id", "prediction"]).T
+result["prediction"] = result["prediction"].map(np.sum)
+result = result.astype(int)
+result.to_csv("prediction_savvy_sea_lion_7.csv", index=False)
 
 
-balance = balanced_accuracy_score(Y_test, Y_pred)
-recall = recall_score(Y_test, Y_pred)
-specificity = 2 * balance - recall
-
-print('accuracy: ' + str(accuracy_score(Y_test, Y_pred)))
-print('balanced accuracy: ' + str(balance))
-print('recall: ' + str(recall))
-print('specificity: ' + str(specificity))
-
-print(df['OFFER_STATUS'].count())
-print(df['OFFER_STATUS'].sum())
-
-"""
-classifiert = RandomForestClassifier(class_weight={1: 1, 0: 4})
-paras = {"n_estimators": [100, 200], "min_samples_split": [0.002, 0.001, 0.004],
-         "min_samples_leaf": [0.001, 0.0005, 0.002, 0.001]}
-cv = GridSearchCV(classifiert, paras, cv=3)
-cv.fit(X_train, Y_train)
-def display(results):
-    print(f'Best parameters are: {results.best_params_}')
-    print("\n")
-    mean_score = results.cv_results_['mean_test_score']
-    std_score = results.cv_results_['std_test_score']
-    params = results.cv_results_['params']
-    for mean,std,params in zip(mean_score,std_score,params):
-        print(f'{round(mean,3)} + or -{round(std,3)} for the {params}')
 
 
-display(cv)
-"""
